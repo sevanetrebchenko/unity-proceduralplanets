66fa46916a113c6b373c52bc67e7e28d8b60a95e
Seva Netrebchenko <seva.netrebchenko@gmail.com>
2020-07-27 17:54:57 -0700

    Created sphere mesh out of a cube with scalable LOD.

---
+++ Assets/Scripts/Planet.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SocialPlatforms;
+
+public class Planet : MonoBehaviour
+{
+    [Range(2,256)]
+    public int resolution = 10;
+
+    [SerializeField, HideInInspector]
+    MeshFilter[] meshFilters;
+
+    TerrainFace[] terrainFaces;
+
+    private void OnValidate()   
+    {
+        Initialize();
+        GenerateMesh();
+    }
+
+    void Initialize()
+    {
+        if (meshFilters == null || meshFilters.Length == 0)
+        {
+            meshFilters = new MeshFilter[6];
+        }
+
+        terrainFaces = new TerrainFace[6];
+
+        // Face directions.
+        Vector3[] directions = { Vector3.up, Vector3.down, Vector3.left, Vector3.right, Vector3.forward, Vector3.back };
+
+        for (int i = 0; i < 6; ++i)
+        {
+            if (meshFilters[i] == null)
+            {
+                GameObject meshObject = new GameObject();
+                meshObject.transform.parent = transform;
+
+                // Initialize mesh filter.
+                meshObject.AddComponent<MeshRenderer>().sharedMaterial = new Material(Shader.Find("Standard"));
+                meshFilters[i] = meshObject.AddComponent<MeshFilter>();
+                meshFilters[i].sharedMesh = new Mesh();
+            }
+
+            // Initialize terrain face.
+            terrainFaces[i] = new TerrainFace(meshFilters[i].sharedMesh, resolution, directions[i]);
+        }
+    }
+
+    void GenerateMesh()
+    {
+        foreach (TerrainFace terrainFace in terrainFaces)
+        {
+            terrainFace.ConstructMesh();
+        }
+    }
+}
---
+++ Assets/Scripts/TerrainFace.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainFace {
+    Mesh mesh;
+    int resolution;
+    Vector3 localUp;
+    Vector3 xAxis;
+    Vector3 yAxis;
+
+    public TerrainFace(Mesh mesh, int resolution, Vector3 localUp) 
+    {
+        this.mesh = mesh;
+        this.resolution = resolution;
+        this.localUp = localUp;
+
+        xAxis = new Vector3(localUp.y, localUp.z, localUp.x);
+        yAxis = Vector3.Cross(localUp, xAxis);
+    }
+
+    public void ConstructMesh()
+    {
+        // Resolution is on one side of the mesh.
+        Vector3[] meshVertices = new Vector3[resolution * resolution];
+
+        // Number of squares in mesh = (resolution - 1) * (resolution - 1)
+        // Number of triangles per square = 2
+        // Number of indices per triangle = 3
+        int[] triangleIndices = new int[(resolution - 1) * (resolution - 1) * 6];
+
+        int triangleIndex = 0;
+
+        for (int y = 0; y < resolution; ++y)
+        {
+            for (int x = 0; x < resolution; ++x)
+            {
+                int vertexIndex = x + (resolution * y);
+                 
+                // Gives the percent position of the vertex along the edge.
+                Vector2 percent = new Vector2(x, y) / (resolution - 1);
+
+                Vector3 pointOnUnitMesh = localUp + (percent.x - 0.5f) * 2 * xAxis + (percent.y - 0.5f) * 2 * yAxis;
+                Vector3 pointOnUnitSphere = pointOnUnitMesh.normalized;
+                meshVertices[vertexIndex] = pointOnUnitSphere;
+
+                // 0        1
+                // ----------
+                // |\       |
+                // | \      |
+                // |  \     |
+                // |   \    |
+                // |    \   |
+                // |     \  |
+                // |      \ |
+                // |       \|
+                // ----------
+                // 2        3
+
+                if (x != resolution - 1 && y != resolution - 1)
+                {
+                    // First triangle. (0, 3, 2)
+                    triangleIndices[triangleIndex] = vertexIndex;
+                    triangleIndices[triangleIndex + 1] = vertexIndex + resolution + 1;
+                    triangleIndices[triangleIndex + 2] = vertexIndex + resolution;
+
+                    // Second triangle. (0, 1, 3)
+                    triangleIndices[triangleIndex + 3] = vertexIndex;
+                    triangleIndices[triangleIndex + 4] = vertexIndex + 1;
+                    triangleIndices[triangleIndex + 5] = vertexIndex + resolution + 1;
+
+                    triangleIndex += 6;
+                }
+            }
+        }
+
+
+        // Assign mesh.
+        mesh.Clear();
+        mesh.vertices = meshVertices;
+        mesh.triangles = triangleIndices;
+        mesh.RecalculateNormals();
+    }
+}
